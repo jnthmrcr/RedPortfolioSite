32177b1b7c482731265b8f0118a1e21cdf14f6fc
Jenith Mercer <jamercer2012@gmail.com>
2022-12-17 14:06:09 -0600

    dont import stuff that was removed

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import Portfolio from './Portfolio'
 // import './index.css'
 import './Portfolio.css'
@@ -8,6 +7,5 @@ import './Portfolio.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Portfolio/>
-	{/* <App/> */}
   </React.StrictMode>
 )
