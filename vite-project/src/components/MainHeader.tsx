72ded6e4e1ef23f8e2191d5f442fb133be6a0ca8
Jenith Mercer <jamercer2012@gmail.com>
2023-03-17 20:24:06 -0500

    main header graphics tweaks navlinks respond to hover, highlight selected link via css

--- vite-project/src/components/MainHeader.tsx
+++ vite-project/src/components/MainHeader.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 interface MainHeaderProps {
@@ -5,20 +6,25 @@ interface MainHeaderProps {
 }
 
 export default function MainHeader({ clickHandler }: MainHeaderProps) {
+
+	let [proj, setProj] = useState<string>("projects");
+	let [abou, setAbou] = useState<string>("about");
+	let [cont, setCont] = useState<string>("contact");
+
 	return (
 		<header className="main-header">
 			<h1>J A Mercer</h1>
 			<div>
 				<p>software developer with experience in react, django, and unity</p>
 				<nav>
-					<NavLink to={'/projects'} onClick={()=>clickHandler('projects')}>
-						projects
+					<NavLink to={'/projects'} onClick={()=>clickHandler('projects')} onMouseEnter={()=>{setProj("view projects")}} onMouseLeave={()=>{setProj("projects")}}>
+						{proj}
 					</NavLink>
-					<NavLink to={'/about'} onClick={()=>clickHandler('about')}>
-						about
+					<NavLink to={'/about'} onClick={()=>clickHandler('about')} onMouseEnter={()=>{setAbou("about me")}} onMouseLeave={()=>{setAbou("about")}}>
+						{abou}
 					</NavLink>
-					<NavLink to={'/contact'} onClick={()=>clickHandler('contact')}>
-						contact
+					<NavLink to={'/contact'} onClick={()=>clickHandler('contact')} onMouseEnter={()=>{setCont("contact me")}} onMouseLeave={()=>{setCont("contact")}}>
+						{cont}
 					</NavLink>
 				</nav>
 			</div>
